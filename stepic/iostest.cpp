1db3fe0f36f0ba5b18fe8f8c161f6fe2b8ee57f6
AidarAlimbayev <transcription@rambler.ru>
2020-01-02 09:22:24 +0600

    commit afet save

---
+++ stepic/iostest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string max;
+    string current;
+    cin >> current;
+    if (current == '\0')
+    cout << current;
+
+    return 0;
+}
--- stepic/word_count.cpp
+++ stepic/word_count.cpp
@@ -7,24 +7,18 @@ using namespace std;
 
 int main() {
     string s;
-    string word_max;
-    string word_temp;
+    string next_word;
+    string max_word;
     int i, n = 0;
-    getline(cin, s);
+    cin >> next_word;
+    while(EOF){
+        if (next_word > max_word){
+            max_word = next_word;
+            cout << max_word << endl;
+        }
+
+    }
     s = s + ' ';
-    cout << s.size() << endl;
-    for (int i = 0; i < s.size(); i++) {
-            word_temp[n] = s[i];
-            cout << word_temp[n];
-            n++;
-                if (s[i] == ' ') {
-                    n = 0;
-                        if (word_temp.size() > word_max.size()){
-                            word_max = word_temp;
-                        }
-                    }
-                    //cout << word_max << endl;
-                }
-    //cout << word_max << endl;
+    
     return 0; 
 }
