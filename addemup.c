61126df59329c407885274ccc13eb513458c183a
AidarAlimbayev <transcription@rambler.ru>
2020-10-09 22:02:54 +0600

    Save changes

---
+++ addemup.c
@@ -0,0 +1,14 @@
+/* #addemup.c -- four kind of statements */
+#include <stdio.h>
+int main(void)
+{
+    int count, sum;
+
+    count = 0;
+    sum = 0;
+    while(count++ < 20)
+        sum = sum + count;
+    printf("sum = &d\n", sum);
+
+    return 0;
+}
